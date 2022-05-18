9a7b381ddf7f00a38084db113963e418f5b3b05c
Sander Wiig <sander.wiig@student.uib.no>
2022-05-18 17:45:21 +0200

    Presentation template & Generics chapter

--- code/GenericsExample.hs
+++ code/GenericsExample.hs
@@ -1,4 +1,4 @@
-module IntrinsicExample where
+module GenericsExample where
 
 type TypeName = String
 type FunName = String
@@ -25,41 +25,100 @@ data ExprAST valuedomain
 
 --DSL Describing Ints
 int ::Signature
-int = (["Int"],[
+int = (["Int", "Bool"],[
                 ("Add", ["Int","Int"],"Int"),
                 ("Mult", ["Int","Int"], "Int"),
                 ("Neg", ["Int"], "Int"),
-                ("Eq", ["Int", "Int"], "Int"),
-                ("LEq", ["Int", "Int"], "Int")
+                ("Eq", ["Int", "Int"], "Bool"),
+                ("LEq", ["Int", "Int"], "Bool")
         ])
 
 --DSL for Describing Reals
 real :: Signature
-real =(["Real"],[
+real =(["Real", "Bool"],[
                 ("Add", ["Real","Real"],"Real"),
                 ("Mult", ["Real","Real"], "Real"),
                 ("Neg", ["Real"],"Real"),
-                ("Eq", ["Real", "Real"], "Real"),
-                ("LEq", ["Real", "Real"], "Real")
+                ("Eq", ["Real", "Real"], "Bool"),
+                ("LEq", ["Real", "Real"], "Bool")
     ])
 --Natural nums
 nat :: Signature 
-nat =(["Nat"],[
+nat =(["Nat","Bool"],[
                 ("Add", ["Nat","Nat"],"Nat"),
                 ("Mult", ["Nat","Nat"], "Nat"),
-                ("Eq", ["Nat", "Nat"], "Nat"),
-                ("LEq", ["Nat", "Nat"], "Nat")
+                ("Eq", ["Nat", "Nat"], "Bool"),
+                ("LEq", ["Nat", "Nat"], "Bool")
     ])
+--Natural nums
+bool :: Signature 
+bool =(["Bool"],[
+            ("Add", ["Bool","Bool"],"Bool"),
+            ("Mult", ["Bool","Bool"], "Bool"),
+            ("Eq", ["Bool", "Bool"], "Bool"),
+            ("LEq", ["Bool", "Bool"], "Bool")
+        ])
 
---Semantics of a ring
-ringModel ::Num valuedomain=>Show valuedomain=>Ord valuedomain=>Eq valuedomain=>FunModel valuedomain
-ringModel "Add" arg =  foldl (+) 0 arg
-ringModel "Mult" arg = foldl (*) 1 arg
-ringModel "Neg" [x] = negate x
-ringModel "Eq" [x, y] | x==y = 1 |otherwise =  0--Emulate booleans wiht int 0=false, 1=true
-ringModel "LEq" [x, y] | x<=y = 1 | otherwise  = 0
+-- | The semantic domain for the rings sigs
+data Ring = BoolDom Bool
+            | NatDom Int Bool
+            | IntDom Int Bool
+            | RealDom Float Bool
+            deriving(Show, Eq, Ord)
 
-eval ::Num valuedomain=>Show valuedomain=>Ord valuedomain=>Eq valuedomain=>FunModel valuedomain->Signature->Enviroment valuedomain->ExprAST valuedomain->valuedomain
+unpackInt :: Ring->Int
+unpackInt (IntDom i _) = i
+unpackNat :: Ring->Int
+unpackNat (NatDom n _) = n
+unpackReal :: Ring->Float
+unpackReal (RealDom x _) = x
+unpackBool :: Ring->Bool
+unpackBool (BoolDom q) = q
+
+getBool ::Ring->Bool
+getBool (IntDom _ q) = q
+getBool (NatDom _ q) = q
+getBool (RealDom _ q) = q
+getBool (BoolDom q) = q
+
+
+-- | Semantics 
+
+--Semantics of a ring(when carrier set is a int)
+intRingModel ::FunModel Ring
+intRingModel "Add" arg =  IntDom (foldl (+) 0 (map unpackInt arg)) False
+intRingModel "Mult" arg = IntDom (foldl (*) 1 (map unpackInt arg)) False
+intRingModel "Neg" [x] = IntDom (negate (unpackInt x)) $ getBool x
+intRingModel "Eq" [x, y] = BoolDom (x==y)
+intRingModel "LEq" [x, y] = BoolDom (x<=y)
+
+--Semantics of a ring(when carrier set is a nat)
+natRingModel ::FunModel Ring
+natRingModel "Add" arg = NatDom  (foldl (+) 0 (map unpackNat arg)) False
+natRingModel "Mult" arg = NatDom (foldl (*) 1 (map unpackNat arg)) False
+natRingModel "Neg" [x] = NatDom (negate (unpackNat x)) $ getBool x
+natRingModel "Eq" [x, y] = BoolDom (x==y)
+natRingModel "LEq" [x, y] =BoolDom (x<=y)
+
+--Semantics of a ring(when carrier set is a real)
+realRingModel ::FunModel Ring
+realRingModel "Add" arg =  RealDom (foldl (+) 0 ((map unpackReal arg))) False
+realRingModel "Mult" arg = RealDom (foldl (*) 1 ((map unpackReal arg))) False
+realRingModel "Neg" [x] = RealDom (negate (unpackReal x))  $ getBool x
+realRingModel "Eq" [x, y] = BoolDom (x==y)
+realRingModel "LEq" [x, y] =BoolDom (x<=y)
+
+--Semantics of a ring(when carrier set is bool)
+boolRingModel ::FunModel Ring
+boolRingModel "Add" arg =  BoolDom $ foldl (/=) False (map unpackBool arg)
+boolRingModel "Mult" arg = BoolDom $ foldl (&&) True (map unpackBool arg)
+boolRingModel "Neg" [x] = BoolDom (not (unpackBool x))
+boolRingModel "Eq" [x, y] = BoolDom (x==y)
+boolRingModel "LEq" [x, y] =BoolDom (x<=y)
+
+
+-- | Eval
+eval ::FunModel Ring->Signature->Enviroment Ring->ExprAST Ring->Ring
 eval mod sig env (Lit i _) = i
 eval mod sig env (Var name) = case lookup name env of
                             Just x -> eval mod sig env x
@@ -68,10 +127,10 @@ eval mod sig env (Var name) = case lookup name env of
 eval mod (types, funcs) env (Fun name args) | elem name (map (\(x,y,z)->x) funcs) =mod name (map (eval mod (types, funcs) env) args)
                                             | otherwise= error "Can't find function in signature"
 
-eval mod (sig) env (Assert expr) = let res = eval mod sig env expr in if res == 1 then res else error $"Assert did not hold"++ (show expr)
+eval mod (sig) env (Assert expr) = let res@(BoolDom b) = eval mod sig env expr in if b then res else error $"Assert did not hold"++ (show expr)
 
 
---Tests that carrier set is abelian group
+-- | Tests that carrier set is abelian group
 testAssoc :: ExprAST valuedomain--We use Integer to represent the set 
 testAssoc = 
     Assert (
@@ -203,5 +262,6 @@ testRDist =
 
 tests = [testAssoc, testAddComm, testAddID, testAddInv, testMultAssoc, testMultID, testLDist, testRDist]
 
-test tpname (sig, model) dta= let env = [("a", Lit (dta!!0) tpname), ("b", Lit (dta!!1) tpname), ("c", Lit (dta!!2) tpname), ("multConst", Lit 1 tpname), ("addConst", Lit 0 tpname)] in
+-- | main
+test tpname (sig, model) dta = let env = [("a", Lit (dta!!0) tpname), ("b", Lit (dta!!1) tpname), ("c", Lit (dta!!2) tpname), ("multConst", Lit (dta!!3) tpname), ("addConst", Lit (dta!!2) tpname)] in
     map (eval model sig env) tests
