e4d509bede1ccdb5e17edfa21c2ce9fc3f2fdf6e
Swift@Laptop <sander.wiig@gmail.com>
2023-05-13 15:12:53 +0200

    Fixed blank page bug.

---
+++ book-of-magne/figures/code/scoping.c
@@ -0,0 +1,22 @@
+// A program to demonstrate static scoping.
+
+int x = 10;
+ 
+// Called by g()
+int f()
+{
+   return x;
+}
+ 
+// g() has its own variable
+// named as x and calls f()
+int g()
+{
+   int x = 20;
+   return f();
+}
+ 
+main()
+{
+  print(g());
+}
